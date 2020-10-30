8f175cd40d8223030b4f9a00700b5019581dc032
JaiVenkateshwaraTechnologies <kuldeephadnoorkat@gmail.com>
2020-10-30 23:49:30 +0530

    ADD UC6 the Storing the Dictionary is done

--- art.sh
+++ art.sh
@@ -1,21 +1,27 @@
 #!/bin/bash -x
 
+declare -a arr
+declare -i dict
+
 read -p "Enter the Digit a - " a
 read -p "Enter the Digit b - " b
 read -p "Enter the Digit c - " c
 
 
 ##first computation
-computation=$(($a+$b*$c))
-echo $computation
+computation[0]=$(($a+$b*$c))
+##echo $computation
 
-computation2=$(($a*$b+$c))
-echo $computation2
+computation[1]=$(($a*$b+$c))
+##echo $computation
 
-computation3=$(($c+(($a/$b))))
-echo $computation3
+computation[2]=$(($c+(($a/$b))))
+##echo $computation
 
-computation4=$((($a%$b)+$c))
-echo $computation4
+computation[3]=$((($a%$b)+$c))
+
+echo ${computation[@]}
+
+
 
 
