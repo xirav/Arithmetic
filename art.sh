67bc7b067ea442ba15dbf6ea2b2639a20fbc16b3
JaiVenkateshwaraTechnologies <kuldeephadnoorkat@gmail.com>
2020-10-31 11:20:33 +0530

    ADD UC8 Ascending order sorting

--- art.sh
+++ art.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
-declare -a arr
-declare -i dict
+
 
 read -p "Enter the Digit a - " a
 read -p "Enter the Digit b - " b
@@ -16,8 +15,22 @@ computation[2]=$(($c+(($a/$b))))
 
 computation[3]=$((($a%$b)+$c))
 ##reading the values from the dict to the array and displaying
-echo ${computation[@]}
+##echo ${computation[@]}
 
 
+for ((i=0 ; i < 5 ; i++ ))
+do
+	for (( j=0 ; j < 5-i-1 ; j++ ))
+	do
+		if [ ${computation[j]} -lt ${computation[$((j+1))]} ]
+		then
+			temp=${computation[$j]}
+			computation[$i]=${arr[$((j+1))]}
+			computation[$((j+1))]=$temp
+		fi
+	done
+done
 
+
+echo ${computation[@]} 
 
